2cc3474e06676bb2ef274299edc4aab9c4792788
Hassan Nawaz <hassancent@gmail.com>
2019-04-13 22:40:10 +0500

    finished all 3 schemes without proper testing

--- OS_Assignment2/Form1.cs
+++ OS_Assignment2/Form1.cs
@@ -10,6 +10,7 @@ namespace OS_Assignment2
     {
         List<RoundTrip> RoundTripList = new List<RoundTrip>();
         List<ShortestRemainingTime> shortestRemainingTimeList = new List<ShortestRemainingTime>();
+        List<Priority> priorityList = new List<Priority>();
         private BindingSource source = new BindingSource();
         private int timeSlice;
         public string itime;
@@ -41,6 +42,14 @@ namespace OS_Assignment2
                 source.ResetBindings(false);
 
             }
+
+            if (typeCombo.SelectedIndex == 2)
+            {
+                priorityList.Add(new Priority("", 0, 0,0));
+                source.DataSource = priorityList;
+                source.ResetBindings(false);
+
+            }
         }
 
         private void typeCombo_SelectedIndexChanged(object sender, EventArgs e)
@@ -48,9 +57,9 @@ namespace OS_Assignment2
             if (typeCombo.SelectedIndex == 0)
             {
                 RoundTripList.Clear();
-                RoundTripList.Add(new RoundTrip("p1", 5, 0));
-                RoundTripList.Add(new RoundTrip("p2", 5, 2));
-                RoundTripList.Add(new RoundTrip("p3", 1, 15));
+                //RoundTripList.Add(new RoundTrip("p1", 5, 0));
+                //RoundTripList.Add(new RoundTrip("p2", 5, 2));
+                //RoundTripList.Add(new RoundTrip("p3", 1, 15));
                 source.ResetBindings(false);
                 source.DataSource = RoundTripList;
                 inputGrid.DataSource = source;
@@ -60,10 +69,21 @@ namespace OS_Assignment2
             if (typeCombo.SelectedIndex == 1)
             {
                 shortestRemainingTimeList.Clear();
-                shortestRemainingTimeList.Add(new ShortestRemainingTime("p1", 4, 0));
-                shortestRemainingTimeList.Add(new ShortestRemainingTime("p2", 1, 1));
+                //shortestRemainingTimeList.Add(new ShortestRemainingTime("p1", 4, 0));
+                //shortestRemainingTimeList.Add(new ShortestRemainingTime("p2", 1, 1));
                 source.ResetBindings(false);
                 source.DataSource = shortestRemainingTimeList;
+                inputGrid.DataSource = source;
+
+            }
+
+            if (typeCombo.SelectedIndex == 2)
+            {
+                priorityList.Clear();
+                //priorityList.Add(new Priority("p1", 4, 0,1));
+                //priorityList.Add(new Priority("p2", 1, 1,2));
+                source.ResetBindings(false);
+                source.DataSource = priorityList;
                 inputGrid.DataSource = source;
 
             }
@@ -91,6 +111,18 @@ namespace OS_Assignment2
             {
                 shortestRemainingTimeList[i].lastSliceEndTime = -1;
                 shortestRemainingTimeList[i].totalWaitingtime = 0;
+                //RoundTripList[i].remainingBrustTime = null;
+
+
+            }
+        }
+
+        public void clearOldDataForP()
+        {
+            for (int i = 0; i < priorityList.Count; i++)
+            {
+                priorityList[i].lastSliceEndTime = -1;
+                priorityList[i].totalWaitingtime = 0;
                 //RoundTripList[i].remainingBrustTime = null;
 
 
@@ -479,6 +511,194 @@ namespace OS_Assignment2
                         }
                     }
 
+                    if (typeCombo.SelectedIndex == 2)
+                    {
+                        //shortest remaining time first
+                        clearOldDataForP();
+                        List<Priority> inputList = priorityList.OrderBy(a => a.ArrivalTime).ThenBy(b => b.PriorityValue).ToList();
+                        List<Priority> currentProcesses = new List<Priority>();
+
+                        //start range, end range, process state, current tick, current process from list, 
+                        //process counter from main list
+                        int sr = 0; int er = 0; string p = "idle";
+                        int i = 0; int cp = 0; int pc = 0;
+
+                        while (true)
+                        {
+                            Priority nextProcess;
+                            if ((pc < inputList.Count))
+                            {
+                                nextProcess = inputList[pc];
+                            }
+                            else
+                            {
+                                nextProcess = inputList.Last();
+                            }
+
+                            if (p.Equals("idle"))
+                            {
+                                //process arrived
+                                if (nextProcess.ArrivalTime <= i)
+                                {
+                                    if (!(sr == 0 && er == 0))
+                                    {
+                                        //this checks if the we are not at 0,0 meaning the tick is probably at 0 or no idle time.
+                                        //log the idle here as well
+                                        result.Add(new ProcessBlock("idle", sr, er, er - sr, 0));
+                                    }
+
+                                    p = nextProcess.ProcessName;
+                                    nextProcess.processTimeSlice.Add(new TimeSlice(i, 0));
+                                    nextProcess.remainingBrustTime = nextProcess.BrustTime;
+                                    currentProcesses.Add(nextProcess);
+                                    pc++;
+                                    if (pc < inputList.Count)
+                                    {
+                                        nextProcess = inputList[pc];
+                                    }
+                                    sr = i;
+                                    er = i;
+                                }
+
+                                //if there are other processes that have arrived
+                                //pc++;
+
+                                while (nextProcess.ArrivalTime <= i && pc < inputList.Count)
+                                {
+                                    nextProcess.remainingBrustTime = nextProcess.BrustTime;
+                                    currentProcesses.Add(nextProcess);
+                                    pc++;
+                                    if ((pc < inputList.Count))
+                                    {
+                                        nextProcess = inputList[pc];
+                                    }
+                                    else
+                                    {
+                                        break;
+                                    }
+
+                                }
+                            }
+                            else
+                            {
+
+                                //a process is already in processor
+                                //check if its slice is done
+                                int expectedEndRange1 = currentProcesses[cp].processTimeSlice.Last().Start + timeSlice;
+                                int expectedEndRange2 = currentProcesses[cp].processTimeSlice.Last().Start + currentProcesses[cp].remainingBrustTime;
+                                //range1 gets full range. like start 0-3 if time slice is 3
+                                //range2 will correct error if brust time is 2 but slice is 3. then expected should be 0-2
+                                //if either one of them is true then process has ended
+                                if (i == expectedEndRange1 || i == expectedEndRange2)
+                                {
+                                    //process/time slice has ended
+                                    Priority currentProcess = currentProcesses[cp];
+                                    currentProcess.processTimeSlice.Last().End = i;
+                                    //add it to results
+                                    int start = currentProcess.processTimeSlice.Last().Start;
+                                    int end = currentProcess.processTimeSlice.Last().End;
+                                    int brust = end - start;
+                                    int wait;
+                                    if (currentProcess.lastSliceEndTime == -1)
+                                    {
+                                        wait = currentProcess.processTimeSlice.Last().Start - currentProcess.ArrivalTime;
+                                        currentProcess.lastSliceEndTime = end;
+                                    }
+                                    else
+                                    {
+                                        wait = start - currentProcess.lastSliceEndTime;
+                                        currentProcess.lastSliceEndTime = end;
+                                    }
+
+                                    result.Add(new ProcessBlock(currentProcess.ProcessName, start,
+                                        end, brust, wait));
+                                    currentProcess.remainingBrustTime = currentProcess.remainingBrustTime - brust;
+
+                                    if (currentProcess.remainingBrustTime == 0)
+                                    {
+                                        //remove it from current process queue
+                                        currentProcesses.RemoveAt(cp);
+                                        //pc++;
+
+                                    }
+                                    //has another process arrived?
+                                    while (nextProcess.ArrivalTime <= i && pc < inputList.Count)
+                                    {
+                                        nextProcess.remainingBrustTime = nextProcess.BrustTime;
+                                        currentProcesses.Add(nextProcess);
+                                        pc++;
+                                        if ((pc < inputList.Count))
+                                        {
+                                            nextProcess = inputList[pc];
+                                        }
+                                        else
+                                        {
+                                            break;
+                                        }
+
+                                    }
+                                    if (currentProcesses.Count != 0)
+                                    {
+                                        //no need for rotation
+                                        //if (cp < currentProcesses.Count - 1)
+                                        //{
+                                        //    cp++;
+                                        //}
+                                        //else
+                                        //{
+                                        //    cp = 0;
+                                        //}
+                                        currentProcesses = currentProcesses.OrderBy(a => a.PriorityValue).ToList();
+                                        cp = 0;
+                                        p = currentProcesses[cp].ProcessName;
+                                        currentProcesses[cp].processTimeSlice.Add(new TimeSlice(i, 0));
+                                        sr = i;
+                                        er = i;
+
+                                    }
+                                    else
+                                    {
+                                        if (pc >= inputList.Count && currentProcesses.Count == 0) break;
+                                        //check if a process has arrived or else set idle
+                                        nextProcess = inputList[pc];
+                                        if (nextProcess.ArrivalTime <= i)
+                                        {
+                                            p = nextProcess.ProcessName;
+                                            nextProcess.processTimeSlice.Add(new TimeSlice(i, 0));
+                                            nextProcess.remainingBrustTime = nextProcess.BrustTime;
+                                            currentProcesses.Add(nextProcess);
+                                            sr = i;
+                                            er = i;
+                                        }
+                                        else
+                                        {
+                                            //set idle
+                                            p = "idle";
+                                            sr = i;
+                                            er = i;
+                                        }
+
+                                    }
+
+
+                                    // if (pc == inputList.Count) break;
+                                    //nextProcess = inputList[pc];
+
+
+                                }
+
+
+
+
+                            }
+
+
+                            er++;
+                            i++;
+
+                        }
+                    }
+
 
                     resultGrid.DataSource = result;
                     //calculate average waiting time
@@ -513,7 +733,7 @@ namespace OS_Assignment2
                     }
                     //try
                     //{
-                        averageWaitTimeLabel.Text = total / RoundTripList.Count + "";
+                        averageWaitTimeLabel.Text = (float) total / (float) RoundTripList.Count + "";
                         //} catch
                         //{
                         //    averageWaitTimeLabel.Text = "N/A";
@@ -540,7 +760,34 @@ namespace OS_Assignment2
                         }
                         //try
                         //{
-                        averageWaitTimeLabel.Text = total / shortestRemainingTimeList.Count + "";
+                        averageWaitTimeLabel.Text = (float) total / (float) shortestRemainingTimeList.Count + "";
+                        //} catch
+                        //{
+                        //    averageWaitTimeLabel.Text = "N/A";
+                        //}
+                    }
+
+                    if (typeCombo.SelectedIndex == 2)
+                    {
+
+
+                        int total = 0;
+                        for (int i = 0; i < priorityList.Count; i++)
+                        {
+                            int count = 0;
+                            foreach (ProcessBlock r in result)
+                            {
+                                if (r.ProcessName.Equals(priorityList[i].ProcessName))
+                                {
+                                    count += r.WaitingTime;
+                                }
+                            }
+                            total += count;
+                            itime += priorityList[i].ProcessName + " : " + count + "\n";
+                        }
+                        //try
+                        //{
+                        averageWaitTimeLabel.Text = (float) total / (float) priorityList.Count + "";
                         //} catch
                         //{
                         //    averageWaitTimeLabel.Text = "N/A";
--- OS_Assignment2/Priority.cs
+++ OS_Assignment2/Priority.cs
@@ -8,5 +8,26 @@ namespace OS_Assignment2
 {
     class Priority
     {
+        private string processName;
+        private int brustTime;
+        private int arrivalTime;
+        private int priorityValue;
+        public List<TimeSlice> processTimeSlice = new List<TimeSlice>();
+        public int remainingBrustTime;
+        public int lastSliceEndTime = -1;
+        public int totalWaitingtime = 0;
+
+        public string ProcessName { get => processName; set => processName = value; }
+        public int BrustTime { get => brustTime; set => brustTime = value; }
+        public int ArrivalTime { get => arrivalTime; set => arrivalTime = value; }
+        public int PriorityValue { get => priorityValue; set => priorityValue = value; }
+
+        public Priority(string processName, int brustTime, int arrivalTime, int priorityValue)
+        {
+            this.processName = processName;
+            this.brustTime = brustTime;
+            this.arrivalTime = arrivalTime;
+            this.priorityValue = priorityValue;
+        }
     }
 }
